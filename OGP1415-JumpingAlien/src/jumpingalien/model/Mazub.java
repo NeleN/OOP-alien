cb10dd506e7845315c24cf50fc960e68c0b537ab
MelanieN <melanie_nijs@hotmail.com>
2015-03-10 11:59:56 +0100

    height and width fixed!

--- OGP1415-JumpingAlien/src/jumpingalien/model/Mazub.java
+++ OGP1415-JumpingAlien/src/jumpingalien/model/Mazub.java
@@ -51,8 +51,8 @@ public class Mazub {
 			setSpeedY(0);
 		if (System.currentTimeMillis() - changedIndex  >= 75)
 			this.updateIndex();
-		getCurrentSprite().getWidth();
-		getCurrentSprite().getHeight();
+		widthMazub = getCurrentSprite().getWidth();
+		heightMazub = getCurrentSprite().getHeight();
 	}
 	
 	public void updateIndex(){
