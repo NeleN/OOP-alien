a56b555d818d7f2a1f882b6134d255a30ce921d6
NeleN <nele.nauwelaers@hotmail.be>
2015-05-19 19:28:30 +0200

    Expressions klaaaar

--- OGP1415-JumpingAlien/src/expression/SearchObject.java
+++ OGP1415-JumpingAlien/src/expression/SearchObject.java
@@ -14,21 +14,46 @@ public class SearchObject extends Expression<Creature> {
 	@Override
 	public Object evaluate() {
 		if (direction == Direction.LEFT){
-			
+			for (Creature creature: getProgram().getUser().getWorld().getInWorldCreatures()){
+				if (positionX > creature.getPositionX() && ((positionX - creature.getPositionX()) < distanceClosestCreature)){
+					closestCreature = creature;
+					distanceClosestCreature = positionX - creature.getPositionX();
+				}
+			}
+			return closestCreature;
 		}
 		if (direction == Direction.RIGHT){
-			
+			for (Creature creature: getProgram().getUser().getWorld().getInWorldCreatures()){
+				if (positionX < creature.getPositionX() && ((creature.getPositionX() - positionX) < distanceClosestCreature)){
+					closestCreature = creature;
+					distanceClosestCreature = creature.getPositionX() - positionX;
+				}
+			}
+			return closestCreature;
 		}
 		if (direction == Direction.DOWN){
-			
+			for (Creature creature: getProgram().getUser().getWorld().getInWorldCreatures()){
+				if (positionY > creature.getPositionY() && ((positionY - creature.getPositionY()) < distanceClosestCreature)){
+					closestCreature = creature;
+					distanceClosestCreature = positionY - creature.getPositionY();
+				}
+			}
+			return closestCreature;
 		}
-		if (direction == Direction.UP){
-			
+		else{																						//if (direction == Direction.UP){
+			for (Creature creature: getProgram().getUser().getWorld().getInWorldCreatures()){
+				if (positionY < creature.getPositionY() && ((creature.getPositionY() - positionY) < distanceClosestCreature)){
+					closestCreature = creature;
+					distanceClosestCreature = creature.getPositionY() - positionY;
+				}
+			}
+			return closestCreature;
 		}
 	}
 	
 	private Direction direction;
 	private double positionX;
 	private double positionY;
-
+	private Creature closestCreature;
+	private double distanceClosestCreature = Double.MAX_VALUE;
 }
